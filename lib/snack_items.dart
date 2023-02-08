25d27268546c4e1046adb12d9437296ca48a7deb
hemensol <hemensol14@yahoo.com>
2023-02-08 20:55:17 +0300

    button feature added

--- lib/snack_items.dart
+++ lib/snack_items.dart
@@ -3,10 +3,12 @@ import 'package:flutter/material.dart';
 class SnackItems extends StatelessWidget {
   final String snackImage;
   final String snackName;
+  final String price;
 
   const SnackItems({
     required this.snackImage,
     required this.snackName,
+    required this.price,
     super.key
     });
 
@@ -14,27 +16,47 @@ class SnackItems extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Container(
-        height: 230,
-        width: MediaQuery.of(context).size.width,
-        padding: const EdgeInsets.all(5),
-        child: Column(
-          children: [
-          Image( image: NetworkImage(snackImage), height: 170),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              children: [
-                Expanded(child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [Text(snackName, style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600)), const SizedBox(width: 3)],
+      child: Padding(
+        padding: const EdgeInsets.all(9.0),
+        child: Container(
+          decoration: BoxDecoration(
+            // border: Border.all(color: Color.fromARGB(255, 58, 58, 58), width: 5),
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [BoxShadow(
+              color: Color.fromARGB(127, 0, 0, 0),
+              blurRadius: 15,
+              spreadRadius: 2
+            )]
+          ),
+          height: 210,
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            children: [
+            Image( image: NetworkImage(snackImage), height: 152),
+            Padding(
+              padding: const EdgeInsets.all(6.0),
+              child: Row(
+                children: [
+                  Expanded(child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [Column(
+                      children: [
+                        Text(snackName, style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600)),
+                        Padding(
+                          padding: const EdgeInsets.all(3.0),
+                          child: Text('$price\$', style: TextStyle(fontSize: 14, fontWeight: FontWeight.w900)),
+                        )
+                      ],
+                    ),  ],
 
-                )),
-              ],
-            ),
-          )
-        ],),
+                  )),
+                ],
+              ),
+            )
+          ],),
+        ),
       ),
     );
   }
--- lib/snacks.dart
+++ lib/snacks.dart
@@ -15,37 +15,37 @@ class Snacks extends StatelessWidget {
         backgroundColor: Colors.white ,
       ),
       body: Padding(
-        padding: const EdgeInsets.only(top: 15.0, right: 10.0, left: 10.0, bottom: 15.0),
+        padding: const EdgeInsets.only(top: 15.0, right: 8.0, left: 8.0, bottom: 15.0),
         child: Center(
           child: Column( 
             children: [
             Row(
               children: const [
               Expanded(
-                child: SnackItems(snackImage: 'https://img.freepik.com/premium-vector/striped-box-container-with-delicious-popcorn-white-background_281653-1140.jpg?w=2000', snackName: 'Pop Corn'),
+                child: SnackItems(snackImage: 'https://img.freepik.com/premium-vector/striped-box-container-with-delicious-popcorn-white-background_281653-1140.jpg?w=2000', snackName: 'Pop Corn', price: '25'),
               ),
               Expanded(
-                child: SnackItems(snackImage: 'https://www.seekpng.com/png/detail/761-7619514_doritos-nacho-cheese-doritos-bag-transparent-background.png', snackName: 'Doritos'),
-              ),
-              ],
-            ),
-            Row(
-              children: const [
-              Expanded(
-                child: SnackItems(snackImage: 'https://media.istockphoto.com/id/458678045/photo/lays-potato-chips.jpg?s=612x612&w=0&k=20&c=vm2Gy6O7wr7PTIkdIntIiBZVUJiisHwyxcK6hYbrNT8=', snackName: 'Lays'),
-              ),
-              Expanded(
-                child: SnackItems(snackImage: 'https://thumbs.dreamstime.com/b/sprite-can-white-background-chisinau-moldova-november-popular-lemon-lime-flavored-soft-drink-created-coca-cola-64145037.jpg', snackName: 'Soda'),
+                child: SnackItems(snackImage: 'https://www.seekpng.com/png/detail/761-7619514_doritos-nacho-cheese-doritos-bag-transparent-background.png', snackName: 'Doritos', price: '20'),
               ),
               ],
             ),
             Row(
               children: const [
               Expanded(
-                child: SnackItems(snackImage: 'https://media.istockphoto.com/id/965515654/photo/pretzels-isolated-on-a-white-background.jpg?s=612x612&w=0&k=20&c=IuGZBfOkBc7rlZxSTaH_1JCtX-66ZyXcn_ibOpKOOOE=', snackName: 'Pretzels'),
+                child: SnackItems(snackImage: 'https://media.istockphoto.com/id/458678045/photo/lays-potato-chips.jpg?s=612x612&w=0&k=20&c=vm2Gy6O7wr7PTIkdIntIiBZVUJiisHwyxcK6hYbrNT8=', snackName: 'Lays', price: '15'),
               ),
               Expanded(
-                child: SnackItems(snackImage: 'https://media.istockphoto.com/id/499621987/photo/cheetos-crunchy-snack.jpg?s=612x612&w=0&k=20&c=uVLL4CLDLZ-zduwBbbBvwQR7fGTb881yfZIX9l5KJpA=', snackName: 'Cheetos'),
+                child: SnackItems(snackImage: 'https://thumbs.dreamstime.com/b/sprite-can-white-background-chisinau-moldova-november-popular-lemon-lime-flavored-soft-drink-created-coca-cola-64145037.jpg', snackName: 'Soda', price: '30'),
+              ),
+              ],
+            ),
+            Row(
+              children: const [
+              Expanded(
+                child: SnackItems(snackImage: 'https://media.istockphoto.com/id/965515654/photo/pretzels-isolated-on-a-white-background.jpg?s=612x612&w=0&k=20&c=IuGZBfOkBc7rlZxSTaH_1JCtX-66ZyXcn_ibOpKOOOE=', snackName: 'Pretzels', price: '50'),
+              ),
+              Expanded(
+                child: SnackItems(snackImage: 'https://media.istockphoto.com/id/499621987/photo/cheetos-crunchy-snack.jpg?s=612x612&w=0&k=20&c=uVLL4CLDLZ-zduwBbbBvwQR7fGTb881yfZIX9l5KJpA=', snackName: 'Cheetos', price: '35'),
               ),
               ],
             ),
